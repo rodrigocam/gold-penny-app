f01503c676332d4888d3f733f93c79bd8f40bb68
rodrigocam <rodrigo.redcode@gmail.com>
2018-04-20 17:06:26 -0300

    Add connection thread to bluetoothmanager

--- app/src/main/java/com/code/red/playvendas/BluetoothManager.java
+++ app/src/main/java/com/code/red/playvendas/BluetoothManager.java
@@ -8,10 +8,12 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Set;
 
 public class BluetoothManager {
-
+    private ConnectThread ct;
     final int REQUEST_ENABLE_BT;
     public BluetoothManager(){
         REQUEST_ENABLE_BT = 10;
@@ -20,7 +22,7 @@ public class BluetoothManager {
     BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
 
-    public void hasPairedDevices(){
+    public void pairedDevices(){
         if (pairedDevices.size() > 0) {
             // Loop through paired devices
             for (BluetoothDevice device : pairedDevices) {
@@ -29,12 +31,13 @@ public class BluetoothManager {
                 Log.d("BLUETOOTH STATUS", device.getName() + "\n" + device.getAddress());
             }
         }
+        
     }
 
     public Boolean getBluetoothStatus() throws Error{
         if(bluetoothAdapter != null){
             if(bluetoothAdapter.isEnabled()) {
-                hasPairedDevices();
+                pairedDevices();
                 return true;
             }else{
                 return false;
@@ -53,13 +56,13 @@ public class BluetoothManager {
         }
     }
 
-    public void startConnection(){
+    public void startConnection() {
         bluetoothAdapter.cancelDiscovery();
-        ConnectThread ct = null;
-        for(BluetoothDevice device : pairedDevices){
-            ct = new ConnectThread(device);
-        }
+        BluetoothDevice device = (BluetoothDevice) pairedDevices.toArray()[0];
+        Log.d("asdsa", "AAEEAAE TEM 1 BAGULHO" + device.getName() + " " + device.getAddress());
+        ct = new ConnectThread(device);
         ct.run();
+
     }
 
 

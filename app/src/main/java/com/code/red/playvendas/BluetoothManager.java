6ed51dce456e00428121594c1fdda09134e31aca
shammyz <rodrigo.redcode@gmail.com>
2018-04-20 15:02:12 -0300

    Add paired devices list

--- app/src/main/java/com/code/red/playvendas/BluetoothManager.java
+++ app/src/main/java/com/code/red/playvendas/BluetoothManager.java
@@ -2,10 +2,13 @@ package com.code.red.playvendas;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
+
+import java.util.Set;
 
 public class BluetoothManager {
 
@@ -15,10 +18,23 @@ public class BluetoothManager {
     }
 
     BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+    public void hasPairedDevices(){
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                Log.d("BLUETOOTH STATUS", device.getName() + "\n" + device.getAddress());
+            }
+        }
+    }
 
     public Boolean getBluetoothStatus() throws Error{
         if(bluetoothAdapter != null){
             if(bluetoothAdapter.isEnabled()) {
+                hasPairedDevices();
                 return true;
             }else{
                 return false;

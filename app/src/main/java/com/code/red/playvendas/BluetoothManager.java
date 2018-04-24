c31537f21de2f30ac7dfe2e61f1b3889832e4b7f
pablodiegoss <pablodiegoss@hotmail.com>
2018-04-24 11:12:20 -0300

    Refactor Bluetooth Manager

--- app/src/main/java/com/code/red/playvendas/BluetoothManager.java
+++ app/src/main/java/com/code/red/playvendas/BluetoothManager.java
@@ -1,69 +1,97 @@
 package com.code.red.playvendas;
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
-import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.View;
 
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Set;
 
 public class BluetoothManager {
+    public final int REQUEST_ENABLE_BT = 10;
     private ConnectThread ct;
-    final int REQUEST_ENABLE_BT;
-    public BluetoothManager(){
-        REQUEST_ENABLE_BT = 10;
-    }
-
+    private final String LOG_TAG = "Bluetooth Manager";
+    
     BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-    Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+    Set<BluetoothDevice> pairedDevices;
 
-    public void pairedDevices(){
-        if (pairedDevices.size() > 0) {
-            // Loop through paired devices
-            for (BluetoothDevice device : pairedDevices) {
-                // Add the name and address to an array adapter to show in a ListView
-                //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-                Log.d("BLUETOOTH STATUS", device.getName() + "\n" + device.getAddress());
-            }
-        }
-        
+    public void updatePairedDevices() {
+        pairedDevices = bluetoothAdapter.getBondedDevices();
+        Log.d(LOG_TAG, pairedDevices.size() + " paired devices");
     }
 
-    public Boolean getBluetoothStatus() throws Error{
-        if(bluetoothAdapter != null){
-            if(bluetoothAdapter.isEnabled()) {
-                pairedDevices();
+    public Boolean isBluetoothOn() throws Error {
+        if (bluetoothAdapter != null) {
+            if (bluetoothAdapter.isEnabled()) {
+                updatePairedDevices();
+                Log.d(LOG_TAG, "Bluetooth on");
                 return true;
-            }else{
+            } else {
+                Log.d(LOG_TAG, "Bluetooth off");
                 return false;
             }
-        }else {
+        } else {
+            Log.e(LOG_TAG, "Bluetooth not available");
             throw new Error("Bluetooth not available");
         }
     }
 
-    public Intent activateBluetooth(){
-        if(!getBluetoothStatus()){
-            Intent enablebtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            return enablebtIntent;
-        }else{
+    public Intent activateBluetooth() {
+        if (!isBluetoothOn()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            return enableBtIntent;
+        } else {
             return null;
         }
     }
-
-    public void startConnection() {
-        bluetoothAdapter.cancelDiscovery();
-        BluetoothDevice device = (BluetoothDevice) pairedDevices.toArray()[0];
-        Log.d("asdsa", "AAEEAAE TEM 1 BAGULHO" + device.getName() + " " + device.getAddress());
-        ct = new ConnectThread(device);
-        ct.run();
-
+    public void writeTextInPrinter(String text) throws Error{
+        startConnection();
+        if(ct != null){
+            ct.write(text);
+        }
+        closeConnection();
     }
 
+    private void startConnection() {
+        // Disables discovery for faster connection
+        bluetoothAdapter.cancelDiscovery();
 
+        BluetoothDevice device = null;
+        try {
+            device = getPrinter();
+        }catch(Error e){
+            Log.e(LOG_TAG, "Printer not found in paired devices");
+        }
+        try{
+            ct = new ConnectThread(device);
+            ct.connect();
+        }catch (Error e){
+            Log.e(LOG_TAG, "No device paired, cant create Connection");
+        }
+    }
+
+    private void closeConnection(){
+        if(ct!= null){
+         ct.disconnect();
+        }
+        // Reactivate discovery after connection
+        bluetoothAdapter.startDiscovery();
+    }
+
+    private BluetoothDevice getPrinter() throws Error{
+        updatePairedDevices();
+
+        // TODO: Verify if this is enough validation to find a printer;
+        BluetoothDevice printer = null;
+        for(BluetoothDevice device : pairedDevices){
+            if(device.getName().toLowerCase().contains("printer")){
+                Log.d(LOG_TAG, device.getName() + " found");
+                return device;
+            }
+        }
+        if(printer == null){
+            throw new Error("Printer not found");
+        }
+        return null;
+    }
 }

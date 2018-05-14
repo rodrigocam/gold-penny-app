ce23e324be480519d31872481ee82f829654af53
pablodiegoss <pablodiegoss@hotmail.com>
2018-05-14 16:34:08 -0300

    Create Product Cache

---
+++ app/src/main/java/com/code/red/playvendas/utils/ProductCache.java
@@ -0,0 +1,14 @@
+package com.code.red.playvendas.utils;
+
+import android.arch.lifecycle.MutableLiveData;
+
+import com.code.red.playvendas.model.Product;
+
+public class ProductCache {
+    public void put(int productId, MutableLiveData<Product> product){
+        // TODO: Simple memory cache, whatever this means...
+    }
+    public MutableLiveData<Product> get(int productId){
+       return null;
+    }
+}

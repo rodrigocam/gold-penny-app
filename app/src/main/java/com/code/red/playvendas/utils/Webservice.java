8bb6a0cebc194444377e306012d8818911dfef9e
pablodiegoss <pablodiegoss@hotmail.com>
2018-05-14 16:34:22 -0300

    Create Web Service

---
+++ app/src/main/java/com/code/red/playvendas/utils/Webservice.java
@@ -0,0 +1,17 @@
+package com.code.red.playvendas.utils;
+
+import com.code.red.playvendas.model.Product;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface Webservice {
+    /**
+     * @GET declares an HTTP GET request
+     * @Path("product") annotation on the productId parameter marks it as a
+     * replacement for the {user} placeholder in the @GET path
+     */
+    @GET("/products/{product}")
+    Call<Product> getUser(@Path("product") int productId);
+}

96ac560e2ac13272a8c5449978e8f75f3f21fbbb
pablodiegoss <pablodiegoss@hotmail.com>
2018-05-27 18:09:10 -0300

    Create obscure classes to try creating ViewModels

---
+++ app/src/main/java/com/code/red/playvendas/viewmodel/ViewModelModule.java
@@ -0,0 +1,30 @@
+package com.code.red.playvendas.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import dagger.Binds;
+import dagger.MapKey;
+import dagger.Module;
+import dagger.multibindings.IntoMap;
+
+@Module
+public abstract class ViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(TokenViewModel.class)
+    abstract ViewModel userViewModel(TokenViewModel tokenViewModel);
+
+
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    @MapKey
+    @interface ViewModelKey {
+        Class<? extends ViewModel> value();
+    }
+}

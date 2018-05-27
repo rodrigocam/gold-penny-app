535dd07cd6cb4485e2d59c41ed05a26cd5edf4eb
pablodiegoss <pablodiegoss@hotmail.com>
2018-05-27 16:29:20 -0300

    Create Token ViewModel

---
+++ app/src/main/java/com/code/red/playvendas/viewmodel/TokenViewModel.java
@@ -0,0 +1,37 @@
+package com.code.red.playvendas.viewmodel;
+
+import android.arch.lifecycle.LiveData;
+import com.code.red.playvendas.model.Token;
+import com.code.red.playvendas.repository.TokenRepository;
+
+import javax.inject.Inject;
+
+public class TokenViewModel {
+    public TokenRepository tokenRepository;
+    public LiveData<Token> token;
+
+    @Inject
+    public TokenViewModel(TokenRepository tokenRepository){
+        this.tokenRepository = tokenRepository;
+    }
+
+    public void init() {
+        if(token == null) {
+            token = tokenRepository.getToken();
+        }
+    }
+
+    public LiveData<Token> getToken() {
+        return token;
+    }
+
+    public void saveToken(){
+        if(token != null){
+            tokenRepository.saveToken(token.getValue());
+        }
+    }
+    public void saveToken(Token token){
+        tokenRepository.saveToken(token);
+        this.token = tokenRepository.getToken();
+    }
+}

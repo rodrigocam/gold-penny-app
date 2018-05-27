5a41d6b21ce20e5af6a4c03bf3dc5c50f3be290f
pablodiegoss <pablodiegoss@hotmail.com>
2018-05-27 16:28:14 -0300

    Create Token Model

---
+++ app/src/main/java/com/code/red/playvendas/dao/TokenDao.java
@@ -0,0 +1,19 @@
+package com.code.red.playvendas.dao;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.code.red.playvendas.model.Token;
+
+import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
+
+@Dao
+public interface TokenDao {
+    @Insert(onConflict = REPLACE)
+    void save(Token token);
+
+    @Query("SELECT * FROM token ORDER BY id DESC LIMIT 1 ")
+    LiveData<Token> load();
+}
--- app/src/main/java/com/code/red/playvendas/database/playdb.java
+++ app/src/main/java/com/code/red/playvendas/database/playdb.java
@@ -4,9 +4,12 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
 import com.code.red.playvendas.dao.ProductDao;
+import com.code.red.playvendas.dao.TokenDao;
 import com.code.red.playvendas.model.Product;
+import com.code.red.playvendas.model.Token;
 
-@Database(entities={Product.class}, version=1)
+@Database(entities={Product.class, Token.class}, version=1)
 public abstract class playdb extends RoomDatabase {
     public abstract ProductDao productDao();
+    public abstract TokenDao tokenDao();
 }

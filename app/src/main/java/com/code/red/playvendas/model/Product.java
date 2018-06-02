2d84bd7f2e248400f231037bdf2725a87ecfa4db
pablodiegoss <pablodiegoss@hotmail.com>
2018-06-02 18:41:00 -0300

    Add Quantity to products

--- app/src/main/java/com/code/red/playvendas/model/Product.java
+++ app/src/main/java/com/code/red/playvendas/model/Product.java
@@ -10,16 +10,17 @@ public class Product {
     private int id;
     private String name;
     private double price;
-
+    private int quantity;
     public Product(){
         super();
     }
 
     @Ignore
-    public Product(int id,String name, Double price){
+    public Product(int id, String name, Double price, int quantity){
         this.id = id;
         this.name = name;
         this.price = price;
+        this.quantity = quantity;
     }
 
     public int getId() {
@@ -46,4 +47,11 @@ public class Product {
         this.price = price;
     }
 
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
